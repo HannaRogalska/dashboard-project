d1ac7928d867ceddb68122f47a7ca6c7b3d595b8
Hanna Rogalska <hanrogalska@gmail.com>
2025-09-09 15:13:25 +0200

    dashboard page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from "@/components/ui/provider";
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider>
+  <BrowserRouter>
+    <StrictMode>
+      <Provider>
         <App />
-    </Provider>
-  </StrictMode>
+      </Provider>
+    </StrictMode>
+  </BrowserRouter>
 );
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <Box p="6" bg="gray.50" minH="100vh">
+      <Heading mb="4">Dashboard</Heading>
+    </Box>
+  );
+}
+
+export default Dashboard
